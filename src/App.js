72ce61272bad3829605ecad80d425be7005f9896
tiagodantasfreire <tiago.freire.2002@gmail.com>
2020-12-28 20:45:16 -0300

    listas com rolagem lateral, responsividade

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React, { useEffect, useState } from 'react'; // useState salva a lista de
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow'
 import './App.css'
+import FeaturedMovie from './components/FeaturedMovie'
+import Header from './components/Header'
 
 export default () => {
 
-    const [movieList, setMovieList] = useState([]); // 
+    const [movieList, setMovieList] = useState([]);
+    const [featuredData, setFeaturedData] = useState(null)
+    const [blackHeader, setBlackHeader] = useState(false)
 
     useEffect(() => {
 
@@ -14,24 +18,63 @@ export default () => {
             // Pega a lista total
             let list = await Tmdb.getHomeList();
             setMovieList(list);
+
+            //Pega o filme em destaque (fetured)
+            let originals = list.filter(i=>i.slug === 'originais');
+            let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+            let chosen = originals[0].items.results[randomChosen];
+            let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv')
+
+            setFeaturedData(chosenInfo)
         }
 
         loadAll();
     }, []);
 
-    // - Header
-    // - Destaque
-    // - Listas
-    // - Rodapé 
+    useEffect(() => {
+        const scrollListener = () => {
+            if (window.scrollY > 10) {
+                setBlackHeader(true)
+            }else{
+                setBlackHeader(false)
+            }
+        }
 
+        window.addEventListener('scroll', scrollListener)
+        return () => {
+            window.removeEventListener('scroll', scrollListener)
+        }
+    }, []);
+
+
+    // Aqui é onde a página é gerada
     return(
         <div className="page">
+
+            <Header black={blackHeader} />
+            
+            {featuredData &&
+                <FeaturedMovie item={featuredData} />
+            }
+
             <section className="lists">
                 {/* Função que retorna as listas */}
-                {movieList.map((item, key) => (
+                {movieList.map((item, key) => ( // só utiliza o key quando é um loop
                     <MovieRow key={key} title={item.title} items={item.items}/> // componente que retorna as listas
                 ))}
             </section>
+
+            <footer>
+                Feito por Tiago Dantas <br/>
+                Direitos de imagem à Netflix <br/>
+                Dados fornecidos pelo site themoviedb.org
+            </footer>
+                    
+            {movieList.length <= 0 && 
+            <div className="loading">
+                <img src="https://www.filmelier.com/pt/br/news/wp-content/uploads/2020/03/netflix-loading.gif" alt="Carregando"/>
+            </div>
+            }
         </div>
     )
 }
--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -1,14 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './MovieRow.css'
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 export default ({title, items}) => {
+    const [scrollX, setScrollX] = useState(0)
+
+    // Rolagem das listas
+    const handleLeftArrow = () => {
+        let x = scrollX + Math.round(window.innerWidth / 2); // Rola a lista de acordo com a metade da tela do usuario
+        if(x > 0){
+            x = 0
+        }
+        setScrollX(x);
+    }
+
+    const handleRigthArrow = () => {
+        let x = scrollX - Math.round(window.innerWidth / 2); // Rola a lista de acordo com a metade da tela do usuario
+        let listW = items.results.length * 150
+        if((window.innerWidth - listW) > x){ // Tamanho da Tela - Tamanho da lista
+            x = (window.innerWidth - listW) - 60 // X = Tamanho da Tela - Tamanho da lista - Padding 
+        }
+        setScrollX(x)
+    }
+
     return (
-        <div>
+        <div className="movierow">
             <h2>{title}</h2>
+            <div className="movierow--left">
+                <NavigateBeforeIcon style={{fontSize: 50}} onClick={handleLeftArrow}/>
+            </div>
+            <div className="movierow--right">
+                <NavigateNextIcon style={{fontSize: 50}} onClick={handleRigthArrow}/>
+            </div>
+
             <div className="movierow--listarea">
-                {items.results.length > 0 && items.results.map((item, key) => (
-                    <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title}/>
-                ))}
+                <div className="movierow--list" style={{
+                marginLeft: scrollX,
+                width: items.results.length * 150
+                }
+                }>
+                    {items.results.length > 0 && items.results.map((item, key) => (
+                        <div key={key} className="movierow--item">
+                            <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title}/>
+                        </div>
+                    ))}
+                </div>
+            
             </div>
         </div>
     );
